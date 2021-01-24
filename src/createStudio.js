ce0a0179feb35dd887d2638379fe3338548d5635
vasilii <porokh.vs@gmail.com>
2021-01-24 17:21:33 +0300

    vingettePass

--- src/createStudio.js
+++ src/createStudio.js
@@ -3,48 +3,21 @@ import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
 import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader';
-import { BokehPass } from 'three/examples/jsm/postprocessing/BokehPass.js';
-import { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass';
 
 //import { VignetteShader } from 'three/examples/jsm/shaders/VignetteShader'
 import { VignetteShaderCustom } from './shaders/VignetteShaderCustom'
 
-
-const CONFIG = {
-    fogData: {
-        color: 0x5B96E7,
-        strength: 0.00057,
-    },
-    amb: {
-        color: 0xffffff,
-        strength: 0.6,
-    },
-    lightDataOne:{
-        color: 0x00FF00,
-        strength: 3,
-        pos: [15, 40, 0],
-    },
-    lightDataTwo: {
-        color: 0xff0000,
-        strength: 5,
-        pos: [-10, -6, 0],
-    },
-}
 
 
 export const createStudio = function () {
     const camera = new THREE.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 1, 10000 );
     //965.1431661754809 444.48798524668956 1043.8103908570088
     camera.position.set( 1133.3833013186731, 351.9369853120578, 1133.4263294071484 ).setLength(1700);
-    //camera.position.set(1133.3833013186731, 4000, 1133.4263294071484).setLength(1700);
-    //camera.rotation.set(0.751582872929487, 0.6294554469014992, 0.5029801008660981);
     const scene = new THREE.Scene();
-
 
 
     const backColor = '#A8D8FF'
     scene.background = new THREE.Color(backColor);
-    //scene.fog = new THREE.FogExp2(backColor, 0.0005 );
     scene.fog = new THREE.Fog( backColor, 500, 3000)
     {
         const light = new THREE.HemisphereLight( 0xffffff, 0x444444, 0.7 );
@@ -58,16 +31,6 @@ export const createStudio = function () {
 
         scene.add(targetObject);
         light.target = targetObject;
-        light.castShadow = false;
-        light.shadow.camera.top =1080;
-        light.shadow.camera.bottom = -1800;
-        light.shadow.camera.left = - 1820;
-        light.shadow.camera.right = 1820;
-
-        light.shadow.mapSize.width = 1000;  // default
-        light.shadow.mapSize.height = 520; // default
-        light.shadow.camera.near = 1;    // default
-        light.shadow.camera.far = 500;     // default
         scene.add( light );
     }
 
@@ -80,19 +43,6 @@ export const createStudio = function () {
     document.getElementById('city').appendChild(renderer.domElement);
 
     const renderPass = new RenderPass( scene, camera )
-
-
-    //const bokehPass = new BokehPass( scene, camera, {
-    //     focus: 1000,
-    //     aperture: 0.00001,
-    //     maxblur: 0.01,
-
-    //     width: window.innerWidth,
-    //     height: window.innerHeight,
-    // } );
-
-    //const effectFilm = new FilmPass( 0.35, 0.75, 2048, 0 );
-
     const composer = new EffectComposer( renderer );
     composer.addPass( renderPass );
 
@@ -107,19 +57,15 @@ export const createStudio = function () {
     composer.addPass(vignettePass)
 
 
-    //composer.addPass( effectFilm );
-    //composer.addPass( bokehPass );
-
-
     const controls = new OrbitControls( camera, renderer.domElement );
     controls.enablePan = true;
-    //controls.minPolarAngle = THREE.Math.degToRad(25);
-    //controls.maxPolarAngle = THREE.Math.degToRad(70);
-    //controls.minDistance = 350;
-    //controls.maxDistance = 1000;
+    controls.minPolarAngle = THREE.Math.degToRad(25);
+    controls.maxPolarAngle = THREE.Math.degToRad(70);
+    controls.minDistance = 350;
+    controls.maxDistance = 1000;
     controls.autoRotate = true;
     controls.autoRotateSpeed = 1;
-    ///controls.target.set( 500, 100, 600 );
+    controls.target.set( 500, 100, 600 );
     ///controls.target.set( 0, 0, 0 );
     controls.update();
 
